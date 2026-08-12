649443e677bf1a50a1d030966ff44261e5cbc45e
bibek698 <mahat6981@gmail.com>
2026-08-12 12:28:35 +0545

    added built-in-functions

--- functions.py
+++ functions.py
@@ -6,6 +6,15 @@ greeting("Bibek", "Engineering")
 
 greeting("Biraj", "Marketing")
 
-greeting("Bibek", "Engineering")
+#built-in functions
 
-greeting("Biraj", "Marketing")
+#type()
+print(type("This is a string"))
+#str()
+number = 12
+string_representation = str(number)
+print(string_representation)
+#sorted()
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+print(time_list)
