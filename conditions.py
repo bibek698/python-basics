cff3fb6474f6674852d06cdd433e772fee2603b0
bibek698 <mahat6981@gmail.com>
2026-08-14 15:16:05 +0545

    added comparison operators

--- conditions.py
+++ conditions.py
@@ -4,3 +4,41 @@ print("cat" == "dog")
 #False
 print (1 != 2)
 #True
+
+print(1 < "1")
+#typeError: str and int cannot be compared
+
+
+print(32 == 30+2)   # The == operator checks if the 2 values are 
+True                # equal to each other. If they are equal, 
+                    # Python returns a True result.
+
+
+print(5+10 == 6+7)  # If the two values are not equal, as in the
+False               # expression 5+10 == 6+7 (or 15 == 13), Python          
+                    # returns a False result.
+
+
+print(10-4 != 10+4) # The != operator checks if the 2 values are
+True                # NOT equal to each other. If true, Python              
+                    # returns a True result. 
+
+
+print(9/3 != 3*1)   # In this last example, 9/3 != 3*1 (or 3 != 3)
+False               # is false. So, Python returns a False value.
+
+# The = equals assignment operator is used to assign a value to a 
+# variable.
+
+my_variable = 3*5           # Assigns a value to my_variable      
+print(my_variable)          # Printing the variable returns the 
+15                          # value assigned to the variable.
+
+
+                              
+# The == equality comparison operator checks if the values of the two
+# expressions on either side of the == operator are equivalent to one 
+# another.
+      
+print(my_variable == 3*5)   # Printing the variable returns a Boolean 
+True                        # True or False result. 
