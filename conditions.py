0acf3b1ee368554f75c9bc80e5860073fef6bd92
bibek698 <mahat6981@gmail.com>
2026-08-14 15:25:27 +0545

    >= <= with strings

--- conditions.py
+++ conditions.py
@@ -167,3 +167,17 @@ print("Five" < 6)
 # Error on line 1:
 #     print("Five" < 6)
 # TypeError: '<' not supported between instances of 'str' and 'int'
+
+
+# Use the Unicode chart in Part 2 to determine if the Unicode values of 
+# the first letters of each string are higher, lower, or equal to one
+# another. 
+
+
+var1 = "my computer" >= "my chair"
+var2 = "Spring" <= "Winter"
+var3 = "pineapple" >= "pineapple"
+ 
+print("Is \"my computer\" greater than or equal to \"my chair\"? Result: ", var1)
+print("Is \"Spring\" less than or equal to \"Winter\"? Result: ", var2)
+print("Is \"pineapple\" less than or equal to \"pineapple\"? Result: ", var3)
