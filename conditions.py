378233c864bb1a59baf00ca639c480d3c7d25457
bibek698 <mahat6981@gmail.com>
2026-08-24 11:25:29 +0545

    refactor hint_username

--- conditions.py
+++ conditions.py
@@ -221,3 +221,12 @@ def is_even(number):
         return True
     return False
 #This code has no output
+
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    else:
+        if len(username) > 15:
+            print("Invalid username. Must be no more than 15 characters long")
+        else:
+            print("Valid username")
