bf35dcad6e6299301a9dcd8134700b0cb68f70f3
bibek698 <mahat6981@gmail.com>
2026-08-24 11:16:53 +0545

    check even with branching

--- conditions.py
+++ conditions.py
@@ -213,3 +213,11 @@ print(not x)
 def hint_username(username):
     if len(username) < 3:
         print("Invalid username. Must be at least 3 characters long")
+    else:
+        print("Valid username")
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+#This code has no output
