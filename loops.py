5a8d4dd45345cf21ebd8c2532cf7f5136a12c6af
bibek698 <mahat6981@gmail.com>
2026-08-29 08:18:40 +0545

    added initializing variables

--- loops.py
+++ loops.py
@@ -12,3 +12,22 @@ def attempts(n):
     print("Done")
 
 attempts(5)
+
+my_variable = 5
+while my_variable < 10:
+    print("Hello")
+    my_variable += 1
+
+x = 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10:
+    product = product * x
+    x = x + 1
+
+print(sum, product)
+#Ouput 45 1
