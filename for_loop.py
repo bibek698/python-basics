d96dc8021614ca15218be5874c110dc55cdd24c4
bibek698 <mahat6981@gmail.com>
2026-08-30 07:23:26 +0545

    added sum and avg of numbers in list

--- for_loop.py
+++ for_loop.py
@@ -4,3 +4,14 @@ for x in range(5):
 friends = ["John", "Bibek", "Sita", "Ram"]
 for friend in friends:
     print("Hello " + friend)
+
+#finding sum and average of numbers in a list
+
+number = [1, 2, 3, 4, 5]
+sum = 0
+length = 0
+for num in number:
+    sum += num
+    length += 1
+print("Sum:", str(sum))
+print("Average:", str(sum / length))
